492f628e3da75739372c72b84347ddc93f96b5a0
martin-sicho <sicho.martin@gmail.com>
2022-06-10 18:02:16 +0200

    attach other nets to devices when reinforcer is initialized

--- drugex/training/trainers.py
+++ drugex/training/trainers.py
@@ -4,6 +4,7 @@ trainers
 Created by: Martin Sicho
 On: 01.06.22, 11:44
 """
+from drugex import DEFAULT_DEVICE_ID, DEFAULT_DEVICE
 from drugex.training.interfaces import Trainer
 
 
@@ -23,6 +24,15 @@ class Reinforcer(Trainer):
 
     def __init__(self, explorer, gpus=(0,)):
         super().__init__(explorer, gpus)
+
+    def attachDevices(self, device_id=DEFAULT_DEVICE_ID, device=DEFAULT_DEVICE):
+        super().attachDevices(device_id=device_id, device=device)
+        if self.model.mutate:
+            self.model.mutate.attachToDevice(self.device)
+            self.model.mutate.attachToDevices(self.getDevices())
+        if self.model.crover:
+            self.model.crover.attachToDevice(self.device)
+            self.model.crover.attachToDevices(self.getDevices())
 
 
     def fit(self, train_loader, valid_loader=None, monitor=None, epochs=1000, args=tuple(), kwargs=dict()):
