d8cd0705903cf0045b4d89f231b4d5210e700e13
martin-sicho <sicho.martin@gmail.com>
2022-06-10 18:01:08 +0200

    add resample option

--- drugex/datasets/interfaces.py
+++ drugex/datasets/interfaces.py
@@ -6,6 +6,9 @@ On: 07.05.22, 15:54
 """
 from abc import ABC, abstractmethod
 
+import numpy as np
+
+from drugex.logs import logger
 from drugex.parallel.interfaces import ResultCollector
 
 
@@ -50,13 +53,24 @@ class DataSet(EncodingCollector, ABC):
     def fromFile(self, path, vocs=tuple(), voc_class=None):
         pass
 
-    def asDataLoader(self, batch_size=None, splitter=None, split_converter=None):
+    def asDataLoader(self, batch_size=None, splitter=None, split_converter=None, n_samples=-1, n_samples_ratio=None):
         if not (split_converter or batch_size):
             raise RuntimeError('You must specify either the batch size or provide a custom split converter.')
         split_converter = split_converter if split_converter else self.getDefaultSplitConverter(batch_size, self.getVoc())
 
+        data = self.getData()
+
+        if n_samples_ratio:
+            n_samples = int(n_samples*n_samples_ratio)
+
+        if n_samples > 0 and n_samples > len(data):
+            logger.info('Replicating original {} fragment-molecule pairs to have set of {} pairs.'.format(len(data), n_samples))
+            data = np.asarray(data)
+            m = int(n_samples/data.shape[0])
+            data = data.repeat(m, axis=0)
+
         results = []
-        for split in self.generateSplits(self.getData(), splitter, split_converter):
+        for split in self.generateSplits(data, splitter, split_converter):
             results.append(split)
 
         if len(results) == 1:
@@ -82,8 +96,8 @@ class DataSet(EncodingCollector, ABC):
 
         return [split_converter(split) if split_converter else split for split in splits]
 
-    def readVocs(self, paths, voc_class):
-        vocs = [voc_class.fromFile(path) for path in paths]
+    def readVocs(self, paths, voc_class, *args, **kwargs):
+        vocs = [voc_class.fromFile(path, *args, **kwargs) for path in paths]
         if len(vocs) > 1:
             voc = sum(vocs[1:], start=vocs[0])
         else:
