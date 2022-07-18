19b075f94d6c857c8ea16970e6814180d32b3645
martin-sicho <sicho.martin@gmail.com>
2022-07-18 09:54:26 +0200

    remove some unused suppliers from the experimental API implementation

--- drugex/molecules/suppliers.py
+++ drugex/molecules/suppliers.py
@@ -35,39 +35,6 @@ class StandardizedSupplier(BaseMolSupplier):
 
         return next(self.mols)
 
-class DataFrameSupplier(BaseMolSupplier):
-
-     def __init__(
-             self,
-             df,
-             mol_col,
-             extra_cols = tuple(),
-             converter=SmilesToDrEx(),
-             hide_duplicates=False
-     ):
-        super().__init__(converter=converter, hide_duplicates=hide_duplicates)
-        df.drop(df.columns.difference(extra_cols + (mol_col,)), 1, inplace=True)
-        self.mols = df.iterrows()
-        self.mol_col = mol_col
-        self.extra_cols = extra_cols
-
-     def next(self):
-         row = next(self.mols)[1]
-         mol = getattr(row, self.mol_col)
-         mol_data = {key : getattr(row, key) for key in self.extra_cols}
-         return mol, mol_data
-
-     def __getstate__(self):
-        d = self.__dict__.copy()
-        if 'mols' in d:
-            d['mols'] = repr(d['mols'])
-        return d
-
-     def __setstate__(self, d):
-        if 'mols' in d:
-            d['mols'] = None
-        self.__dict__.update(d)
-
 class  ListSupplier(BaseMolSupplier):
     """
     Basic supplier that converts molecules in a list to the desired representation (SMILES string to `DrExMol` by default).
---
+++ drugex/parallel/tests.py
@@ -0,0 +1,57 @@
+"""
+tests
+
+Created by: Martin Sicho
+On: 12.04.22, 15:07
+"""
+import os
+from unittest import TestCase
+
+import pandas as pd
+
+from drugex.molecules.mol import DrExMol
+from drugex.molecules.suppliers import ListSupplier
+from drugex.parallel.evaluator import ParallelSupplierEvaluator
+
+
+class TestFileParsers(TestCase):
+
+    @staticmethod
+    def getTestFile(name):
+        return f'{os.path.dirname(__file__)}/test_files/{name}'
+
+    def test_parallel(self):
+        smiles = pd.read_csv(self.getTestFile('test.tsv'), sep="\t", header=0).CANONICAL_SMILES.tolist()
+        para_supplier = ParallelSupplierEvaluator(
+            ListSupplier,
+            n_proc=2,
+            chunk_size=int(len(smiles) / 2)
+        )
+
+        ret = []
+        def collect(results):
+            results = results[0]
+            for mol in results:
+                ret.append(mol)
+                self.assertTrue(isinstance(mol, DrExMol))
+
+        para_supplier.apply(smiles, collect)
+        self.assertTrue(len(ret) == 10)
+
+    def test_parallel_with_suppliers(self):
+        smiles = pd.read_csv(self.getTestFile('test.tsv'), sep="\t", header=0).CANONICAL_SMILES.tolist()
+        para_supplier = ParallelSupplierEvaluator(
+            ListSupplier,
+            n_proc=2
+        )
+
+        def collect(ret):
+            result = ret[0]
+            supplier = ret[1]
+            self.assertTrue(len(result) > 0)
+            self.assertTrue(isinstance(supplier, ListSupplier))
+            self.assertTrue(len(result) == 5)
+            for mol in result:
+                self.assertTrue(isinstance(mol, DrExMol))
+
+        para_supplier.apply(smiles, collect)
