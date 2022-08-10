8e19f7144a2d9b12177ee99d34dbdec281f31a3b
maagdenbehwvanden1 <h.w.van.den.maagdenberg@lacdr.leidenuniv.nl>
2022-08-10 10:53:24 +0200

    restructured classifier for parameter grid search

--- drugex/environment/classifier.py
+++ drugex/environment/classifier.py
@@ -4,15 +4,23 @@ from torch import nn
 from torch import optim
 from torch.nn import functional as F
 import time
-from drugex import utils
+import drugex
+import inspect
+from collections import defaultdict
 
 
 class Base(nn.Module):
     """ This class is the base structure for all of classification/regression DNN models.
-    Mainly, it provides the general methods for training, evaluating model and predcting the given data.
+    Mainly, it provides the general methods for training, evaluating model and predicting the given data.
     """
 
-    def fit(self, train_loader, valid_loader, out, epochs=100, lr=1e-4):
+    def __init__(self, n_epochs=100, lr=1e-4):
+        super().__init__()
+        self.n_epochs = n_epochs,
+        self.lr = lr
+        self.dev = drugex.DEFAULT_DEVICE
+
+    def fit(self, train_loader, valid_loader, out):
         """Training the DNN model, similar to the scikit-learn or Keras style.
         In the end, the optimal value of parameters will also be persisted on the hard drive.
         Arguments:
@@ -30,27 +38,27 @@ class Base(nn.Module):
         if 'optim' in self.__dict__:
             optimizer = self.optim
         else:
-            optimizer = optim.Adam(self.parameters(), lr=lr)
+            optimizer = optim.Adam(self.parameters(), lr=self.lr)
 
         # record the minimum loss value based on the calculation of loss function by the current epoch
         best_loss = np.inf
         # record the epoch when optimal model is saved.
         last_save = 0
-        log = open(out + '.log', 'w')
-        for epoch in range(epochs):
+        log = open(out + '.log', 'a')
+        for epoch in range(self.n_epochs):
             t0 = time.time()
             for param_group in optimizer.param_groups:
-                param_group['lr'] = lr * (1 - 1 / epochs) ** (epoch * 10)
+                param_group['lr'] = self.lr * (1 - 1 / self.n_epochs) ** (epoch * 10)
             for i, (Xb, yb) in enumerate(train_loader):
                 # Batch of target tenor and label tensor
-                Xb, yb = Xb.to(utils.dev), yb.to(utils.dev)
+                Xb, yb = Xb.to(self.dev), yb.to(self.dev)
                 optimizer.zero_grad()
                 # predicted probability tensor
                 y_ = self(Xb, istrain=True)
                 # ignore all of the NaN values
                 ix = yb == yb
                 yb, y_ = yb[ix], y_[ix]
-                wb = torch.Tensor(yb.size()).to(utils.dev)
+                wb = torch.Tensor(yb.size()).to(self.dev)
                 wb[yb == 3.99] = 0.1
                 wb[yb != 3.99] = 1
                 # loss function calculation based on predicted tensor and label tensor
@@ -60,7 +68,7 @@ class Base(nn.Module):
             # loss value on validation set based on which optimal model is saved.
             loss_valid = self.evaluate(valid_loader)
             print('[Epoch: %d/%d] %.1fs loss_train: %f loss_valid: %f' % (
-                epoch, epochs, time.time() - t0, loss.item(), loss_valid), file=log)
+                epoch, self.n_epochs, time.time() - t0, loss.item(), loss_valid), file=log)
             if loss_valid < best_loss:
                 torch.save(self.state_dict(), out + '.pkg')
                 print('[Performance] loss_valid is improved from %f to %f, Save model to %s' %
@@ -72,6 +80,7 @@ class Base(nn.Module):
                 # early stopping, if the performance on validation is not improved in 100 epochs.
                 # The model training will stop in order to save time.
                 if epoch - last_save > 100: break
+        print('Neural net fitting completed.', file=log)
         log.close()
         self.load_state_dict(torch.load(out + '.pkg'))
 
@@ -86,11 +95,11 @@ class Base(nn.Module):
         """
         loss = 0
         for Xb, yb in loader:
-            Xb, yb = Xb.to(utils.dev), yb.to(utils.dev)
+            Xb, yb = Xb.to(self.dev), yb.to(self.dev)
             y_ = self.forward(Xb)
             ix = yb == yb
             yb, y_ = yb[ix], y_[ix]
-            wb = torch.Tensor(yb.size()).to(utils.dev)
+            wb = torch.Tensor(yb.size()).to(self.dev)
             wb[yb == 3.99] = 0.1
             wb[yb != 3.99] = 1
             loss += self.criterion(y_ * wb, yb * wb).item()
@@ -108,13 +117,94 @@ class Base(nn.Module):
                 it is a m X l FloatTensor (m is the No. of sample, l is the No. of classes or tasks.)
         """
         score = []
-        for Xb, yb in loader:
-            Xb = Xb.to(utils.dev)
+        for Xb, _ in loader:
+            Xb = Xb.to(self.dev)
             y_ = self.forward(Xb)
             score.append(y_.detach().cpu())
         score = torch.cat(score, dim=0).numpy()
         return score
 
+    @classmethod
+    def _get_param_names(cls):
+        # function copy from sklearn.base_estimator
+        # get the class parameter names
+        init_signature = inspect.signature(cls.__init__)
+        parameters = [
+            p
+            for p in init_signature.parameters.values()
+            if p.name != "self" and p.kind != p.VAR_KEYWORD
+        ]
+        return sorted([p.name for p in parameters])
+    
+    def get_params(self, deep=True):
+        # function copy from sklearn.base_estimator
+        """
+        Get parameters for this estimator.
+
+        Parameters
+        ----------
+        deep : bool, default=True
+            If True, will return the parameters for this estimator and
+            contained subobjects that are estimators.
+
+        Returns
+        -------
+        params : dict
+            Parameter names mapped to their values.
+        """
+        out = dict()
+        for key in self._get_param_names():
+            value = getattr(self, key)
+            if deep and hasattr(value, "get_params"):
+                deep_items = value.get_params().items()
+                out.update((key + "__" + k, val) for k, val in deep_items)
+            out[key] = value
+        return out
+    
+    def set_params(self, **params):
+        # function copy from sklearn.base_estimator
+        """Set the parameters of this estimator.
+
+        The method works on simple estimators as well as on nested objects
+        (such as :class:`~sklearn.pipeline.Pipeline`). The latter have
+        parameters of the form ``<component>__<parameter>`` so that it's
+        possible to update each component of a nested object.
+
+        Parameters
+        ----------
+        **params : dict
+            Estimator parameters.
+
+        Returns
+        -------
+        self : estimator instance
+            Estimator instance.
+        """
+        if not params:
+            # Simple optimization to gain speed (inspect is slow)
+            return self
+        valid_params = self.get_params(deep=True)
+
+        nested_params = defaultdict(dict)  # grouped by prefix
+        for key, value in params.items():
+            key, delim, sub_key = key.partition("__")
+            if key not in valid_params:
+                local_valid_params = self._get_param_names()
+                raise ValueError(
+                    f"Invalid parameter {key!r} for estimator {self}. "
+                    f"Valid parameters are: {local_valid_params!r}."
+                )
+
+            if delim:
+                nested_params[key][sub_key] = value
+            else:
+                setattr(self, key, value)
+                valid_params[key] = value
+
+        for key, sub_params in nested_params.items():
+            valid_params[key].set_params(**sub_params)
+
+        return self
 
 class STFullyConnected(Base):
     """Single task DNN classification/regression model. It contains four fully connected layers between which
@@ -123,20 +213,35 @@ class STFullyConnected(Base):
         n_dim (int): the No. of columns (features) for input tensor
         n_class (int): the No. of columns (classes) for output tensor.
         is_reg (bool, optional): Regression model (True) or Classification model (False)
+        n_eopchs (int): max number of epochs
+        lr (float): neural net learning rate
+        neurons_h1 (int): number of neurons in first hidden layer
+        neurons_hx (int): number of neurons in other hidden layers
+        extra_layer (bool): add third hidden layer
     """
 
-    def __init__(self, n_dim, n_class, is_reg=False):
-        super(STFullyConnected, self).__init__()
-        self.dropout = nn.Dropout(0.25)
-        self.fc0 = nn.Linear(n_dim, 4000)
-        self.fc1 = nn.Linear(4000, 1000)
-        # self.fc2 = nn.Linear(2000, 1000)
-        self.fc3 = nn.Linear(1000, n_class)
+    def __init__(self, n_dim, n_class, n_epochs = 100, lr = 1e-4, is_reg=False, neurons_h1 = 4000, neurons_hx = 1000, extra_layer = False):
+        super().__init__(n_epochs, lr)
+        self.n_dim = n_dim
+        self.n_class = n_class
         self.is_reg = is_reg
-        if is_reg:
+        self.neurons_h1 = neurons_h1
+        self.neurons_hx = neurons_hx
+        self.extra_layer = extra_layer
+        self.init_model()
+
+    def init_model(self):
+        self.dropout = nn.Dropout(0.25)
+        self.fc0 = nn.Linear(self.n_dim, self.neurons_h1)
+        self.fc1 = nn.Linear(self.neurons_h1, self.neurons_hx)
+        if self.extra_layer:
+            self.fc2 = nn.Linear(self.neurons_hx, self.neurons_hx)
+        self.fc3 = nn.Linear(self.neurons_hx, self.n_class)
+        self.is_reg = self.is_reg
+        if self.is_reg:
             # loss function for regression
             self.criterion = nn.MSELoss()
-        elif n_class == 1:
+        elif self.n_class == 1:
             # loss function and activation function of output layer for binary classification
             self.criterion = nn.BCELoss()
             self.activation = nn.Sigmoid()
@@ -144,7 +249,11 @@ class STFullyConnected(Base):
             # loss function and activation function of output layer for multiple classification
             self.criterion = nn.CrossEntropyLoss()
             self.activation = nn.Softmax()
-        self.to(utils.dev)
+        self.to(self.dev)
+
+    def set_params(self, **params):
+        super().set_params(**params)
+        self.init_model()
 
     def forward(self, X, istrain=False):
         """Invoke the class directly as a function
@@ -158,9 +267,10 @@ class STFullyConnected(Base):
         if istrain:
             y = self.dropout(y)
         y = F.relu(self.fc1(y))
-        # if istrain:
-        #     y = self.dropout(y)
-        # y = F.relu(self.fc2(y))
+        if self.extra_layer:
+            if istrain:
+                y = self.dropout(y)
+            y = F.relu(self.fc2(y))
         if istrain:
             y = self.dropout(y)
         if self.is_reg:
@@ -169,93 +279,93 @@ class STFullyConnected(Base):
             y = self.activation(self.fc3(y))
         return y
 
+# TODO: implement multi-task DNN
+# class MTFullyConnected(Base):
+#     """Multi-task DNN classification/regression model. It contains four fully connected layers
+#     between which are dropout layer for robustness.
+#     Arguments:
+#         n_dim (int): the No. of columns (features) for input tensor
+#         n_task (int): the No. of columns (tasks) for output tensor.
+#         is_reg (bool, optional): Regression model (True) or Classification model (False)
+#     """
 
-class MTFullyConnected(Base):
-    """Multi-task DNN classification/regression model. It contains four fully connected layers
-    between which are dropout layer for robustness.
-    Arguments:
-        n_dim (int): the No. of columns (features) for input tensor
-        n_task (int): the No. of columns (tasks) for output tensor.
-        is_reg (bool, optional): Regression model (True) or Classification model (False)
-    """
+#     def __init__(self, n_dim, n_task, is_reg=False):
+#         super(MTFullyConnected, self).__init__()
+#         self.n_task = n_task
+#         self.dropout = nn.Dropout(0.25)
+#         self.fc0 = nn.Linear(n_dim, 4000)
+#         self.fc1 = nn.Linear(4000, 2000)
+#         self.fc2 = nn.Linear(2000, 1000)
+#         self.output = nn.Linear(1000, n_task)
+#         self.is_reg = is_reg
+#         if is_reg:
+#             # loss function for regression
+#             self.criterion = nn.MSELoss()
+#         else:
+#             # loss function and activation function of output layer for multiple classification
+#             self.criterion = nn.BCELoss()
+#             self.activation = nn.Sigmoid()
+#         self.to(self.dev)
 
-    def __init__(self, n_dim, n_task, is_reg=False):
-        super(MTFullyConnected, self).__init__()
-        self.n_task = n_task
-        self.dropout = nn.Dropout(0.25)
-        self.fc0 = nn.Linear(n_dim, 4000)
-        self.fc1 = nn.Linear(4000, 2000)
-        self.fc2 = nn.Linear(2000, 1000)
-        self.output = nn.Linear(1000, n_task)
-        self.is_reg = is_reg
-        if is_reg:
-            # loss function for regression
-            self.criterion = nn.MSELoss()
-        else:
-            # loss function and activation function of output layer for multiple classification
-            self.criterion = nn.BCELoss()
-            self.activation = nn.Sigmoid()
-        self.to(utils.dev)
+#     def forward(self, X, istrain=False):
+#         """Invoke the class directly as a function
+#         Arguments:
+#             X (FloatTensor): m X n FloatTensor, m is the No. of samples, n is the No. of features.
+#             istrain (bool, optional): is it invoked during training process (True)
+#                 or just for prediction (False)
+#         Return:
+#             y (FloatTensor): m X l FloatTensor, m is the No. of samples, n is the No. of tasks
+#         """
+#         y = F.relu(self.fc0(X))
+#         if istrain:
+#             y = self.dropout(y)
+#         y = F.relu(self.fc1(y))
+#         if istrain:
+#             y = self.dropout(y)
+#         y = F.relu(self.fc2(y))
+#         if istrain:
+#             y = self.dropout(y)
+#         if self.is_reg:
+#             y = self.output(y)
+#         else:
+#             y = self.activation(self.output(y))
+#         return y
 
-    def forward(self, X, istrain=False):
-        """Invoke the class directly as a function
-        Arguments:
-            X (FloatTensor): m X n FloatTensor, m is the No. of samples, n is the No. of features.
-            istrain (bool, optional): is it invoked during training process (True)
-                or just for prediction (False)
-        Return:
-            y (FloatTensor): m X l FloatTensor, m is the No. of samples, n is the No. of tasks
-        """
-        y = F.relu(self.fc0(X))
-        if istrain:
-            y = self.dropout(y)
-        y = F.relu(self.fc1(y))
-        if istrain:
-            y = self.dropout(y)
-        y = F.relu(self.fc2(y))
-        if istrain:
-            y = self.dropout(y)
-        if self.is_reg:
-            y = self.output(y)
-        else:
-            y = self.activation(self.output(y))
-        return y
+# TODO: Is this model useful?
+# class Discriminator(Base):
+#     """A CNN for text classification
+#     architecture: Embedding >> Convolution >> Max-pooling >> Softmax
+#     """
 
+#     def __init__(self, vocab_size, emb_dim, filter_sizes, num_filters, dropout=0.25):
+#         super(Discriminator, self).__init__()
+#         self.emb = nn.Embedding(vocab_size, emb_dim)
+#         self.convs = nn.ModuleList([
+#             nn.Conv2d(1, n, (f, emb_dim)) for (n, f) in zip(num_filters, filter_sizes)
+#         ])
+#         self.highway = nn.Linear(sum(num_filters), sum(num_filters))
+#         self.dropout = nn.Dropout(p=dropout)
+#         self.lin = nn.Linear(sum(num_filters), 1)
+#         self.sigmoid = nn.Sigmoid()
+#         self.init_parameters()
 
-class Discriminator(Base):
-    """A CNN for text classification
-    architecture: Embedding >> Convolution >> Max-pooling >> Softmax
-    """
+#         self.to(self.dev)
+#         self.optim = torch.optim.Adam(filter(lambda p: p.requires_grad, self.parameters()))
 
-    def __init__(self, vocab_size, emb_dim, filter_sizes, num_filters, dropout=0.25):
-        super(Discriminator, self).__init__()
-        self.emb = nn.Embedding(vocab_size, emb_dim)
-        self.convs = nn.ModuleList([
-            nn.Conv2d(1, n, (f, emb_dim)) for (n, f) in zip(num_filters, filter_sizes)
-        ])
-        self.highway = nn.Linear(sum(num_filters), sum(num_filters))
-        self.dropout = nn.Dropout(p=dropout)
-        self.lin = nn.Linear(sum(num_filters), 1)
-        self.sigmoid = nn.Sigmoid()
-        self.init_parameters()
+#     def forward(self, x):
+#         """
+#         Args:
+#             x: (batch_size * seq_len)
+#         """
+#         emb = self.emb(x).unsqueeze(1)  # batch_size * 1 * seq_len * emb_dim
+#         convs = [F.relu(conv(emb)).squeeze(3) for conv in self.convs]  # [batch_size * num_filter * length]
+#         pools = [F.max_pool1d(conv, conv.size(2)).squeeze(2) for conv in convs]  # [batch_size * num_filter]
+#         pred = torch.cat(pools, 1)  # batch_size * num_filters_sum
+#         highway = self.highway(pred)
+#         pred = highway.sigmoid() * F.relu(highway) + (1. - highway.sigmoid()) * pred
+#         pred = self.sigmoid(self.lin(self.dropout(pred)))
+#         return pred
 
-        self.to(utils.dev)
-        self.optim = torch.optim.Adam(filter(lambda p: p.requires_grad, self.parameters()))
-
-    def forward(self, x):
-        """
-        Args:
-            x: (batch_size * seq_len)
-        """
-        emb = self.emb(x).unsqueeze(1)  # batch_size * 1 * seq_len * emb_dim
-        convs = [F.relu(conv(emb)).squeeze(3) for conv in self.convs]  # [batch_size * num_filter * length]
-        pools = [F.max_pool1d(conv, conv.size(2)).squeeze(2) for conv in convs]  # [batch_size * num_filter]
-        pred = torch.cat(pools, 1)  # batch_size * num_filters_sum
-        highway = self.highway(pred)
-        pred = highway.sigmoid() * F.relu(highway) + (1. - highway.sigmoid()) * pred
-        pred = self.sigmoid(self.lin(self.dropout(pred)))
-        return pred
-
-    def init_parameters(self):
-        for param in self.parameters():
-            param.data.uniform_(-0.05, 0.05)
+#     def init_parameters(self):
+#         for param in self.parameters():
+#             param.data.uniform_(-0.05, 0.05)
