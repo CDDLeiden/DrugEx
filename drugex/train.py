79bdf309456ba5a61931a2205932a86fe9dd4e28
maagdenbehwvanden1 <h.w.van.den.maagdenberg@lacdr.leidenuniv.nl>
2022-08-31 14:10:46 +0200

    change to pretrained model path

--- drugex/train.py
+++ drugex/train.py
@@ -36,7 +36,7 @@ def GeneratorArgParser(txt=None):
     parser.add_argument('-i', '--input', type=str, default=None,
                         help="Full file name of input file used both as train and validation sets OR common prefix of train and validation set input files.")  
     parser.add_argument('-vfs', '--voc_files', type=str, nargs='*', default=None,
-                        help="Names of voc files to use as vocabulary. If None, uses the input prefix.")
+                        help="Prefix of voc files to use as vocabulary ({}_smiles/graph.txt). If None, assumes no prefix.")
     parser.add_argument('-o', '--output', type=str, default=None,
                         help="Prefix of output files. If None, set to be the first word of input. ")     
     parser.add_argument('-m', '--mode', type=str, default='RL',
@@ -59,7 +59,8 @@ def GeneratorArgParser(txt=None):
     parser.add_argument('-a', '--algorithm', type=str, default='trans',
                         help="Generator algorithm: 'trans' for (graph/smiles, transformer) or "\
                              "'ved' (smiles, lstm-based encoder-decoder) or "\
-                             "'attn' (smiles, lstm-based encoder-decoder with attention mechanism) ")
+                             "'attn' (smiles, lstm-based encoder-decoder with attention mechanism) " \
+                             "If version = 2, then always 'rnn' is used")
     parser.add_argument('-e', '--epochs', type=int, default=1000,
                         help="Number of epochs")
     parser.add_argument('-bs', '--batch_size', type=int, default=256,
@@ -479,9 +480,9 @@ def FineTune(args):
     """
     
     if args.pretrained_model :
-        if args.pretrained_model.endswith('.pkg'):
-            pt_path = os.path.join(args.base_dir, 'generators', args.pretrained_model)
-        else:
+        pt_path = os.path.join(args.base_dir, 'generators', args.pretrained_model +'.pkg')
+        if not os.path.exists(pt_path):
+            log.warning('%s does not exist, trying %s as prefix of path name.' % (pt_path, args.pretrained_model))
             pt_path = os.path.join(args.base_dir, 'generators', '_'.join([args.pretrained_model, args.mol_type, args.algorithm, 'PT']) +'.pkg')
         assert os.path.exists(pt_path), f'{pt_path} does not exist'
     else:
