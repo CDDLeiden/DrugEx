30ab3ae98ced13b05a700b3dac9b8110bc61ac9a
sohvi <s.luukkonen@lacdr.leidenuniv.nl>
2022-09-27 18:13:22 +0200

    simplify dummy molecule creation

--- drugex/molecules/converters/dummy_molecules.py
+++ drugex/molecules/converters/dummy_molecules.py
@@ -11,65 +11,53 @@ from drugex.logs import logger
 
 class dummyMolsFromFragments():
 
-    def addCBrToFrag(self, frag):
+    def addCBrToFragments(self, frag):
 
         repl = Chem.MolFromSmiles('CBr')
         patt = Chem.MolFromSmarts('[#1;$([#1])]')   
 
-        fragH = Chem.AddHs(Chem.MolFromSmiles(frag))   
-        molH = Chem.ReplaceSubstructs(fragH, patt, repl, replaceAll=False)
-        mol = Chem.RemoveHs(molH[0])
-        return Chem.MolToSmiles(mol)
+        try:
+            fragH = Chem.AddHs(Chem.MolFromSmiles(frag))   
+            molH = Chem.ReplaceSubstructs(fragH, patt, repl, replaceAll=False)
+            mol = Chem.RemoveHs(molH[0])
+            return Chem.MolToSmiles(mol)
+        except:
+            logger.debug(f"Skipped: couldn't build a molecule from {frag}.")
+            return None
 
-    def createMolsFromSingleFragments(self, frags):  
+    def bridgeFragments(self, frags):
 
-        pairs = []
-        for frag in frags:
-            try:
-                smiles = self.addCBrToFrag(frag)
-                pairs.append((frag, smiles))
-            except:
-                logger.debug(f"Skipped: couldn't build a molecule from {frag}.")
+        try:
+            frags_rdkit = [Chem.MolFromSmiles(f) for f in frags.split('.') ]
+            mol = frags_rdkit[0]
+            for i in range(1,len(frags_rdkit)):
+                n = mol.GetNumAtoms()
+                comb = Chem.EditableMol( Chem.CombineMols(mol, frags_rdkit[i]))
+                comb.AddBond(n-1, n, order=Chem.rdchem.BondType.SINGLE)
+                mol = comb.GetMol()
+            return Chem.MolToSmiles(mol)
+        except:
+            logger.debug(f"Skipped: couldn't build a molecule from {frags}.")
+            return None
+            
 
-        return pairs
-
-    def createMolsFromMultipleFragments(self, frags_list):
-
-        pairs=[]
-        for frags in frags_list:
-            try:
-                frags_rdkit = [Chem.MolFromSmiles(f) for f in frags.split('.') ]
-                mol = frags_rdkit[0]
-                for i in range(1,len(frags_rdkit)):
-                    n = mol.GetNumAtoms()
-                    comb = Chem.EditableMol( Chem.CombineMols(mol, frags_rdkit[i]))
-                    comb.AddBond(n-1, n, order=Chem.rdchem.BondType.SINGLE)
-                    mol = comb.GetMol()
-                pairs.append( (frags, Chem.MolToSmiles(mol)))
-            except:
-                print(frags)
-        
-        return pairs
-                    
-
-    def __call__(self, frags):
+    def __call__(self, frag):
 
         """ 
-        Create molecules from list of by adding CBr to single fragments and bridge multiple fragment
+        Create molecule from by adding CBr to single fragments or bridge multiple fragment
         
         Args:
-            frags : SMILES of fragments
+            frags : SMILES of fragment(s)
         Return:
             a list of `tuple`s of format  (fragment, smiles), fragment is the same as the input in "fragments"
         """
 
-        single_frags = [f for f in frags if '.' not in f]
-        multiple_frags = [f for f in frags if '.' in f]
-
-        pairs = []
-        if single_frags:
-            pairs += self.createMolsFromSingleFragments(single_frags)
-        if multiple_frags:
-            pairs += self.createMolsFromMultipleFragments(multiple_frags)
-
-        return pairs
+        try:
+            if '.' in frag: # multiple leaf fragments
+                smiles = self.bridgeFragments(frag)
+            else: # single leaf fragment
+                smiles = self.addCBrToFragments(frag)
+            return (frag, smiles)
+        except:
+            logger.warning(f"Skipped: couldn't build a molecule from {frag}.")
+            return None     
