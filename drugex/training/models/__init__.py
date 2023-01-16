0d2d57110bd12b9df48a5f1d0b627c78922f4022
sohvi <s.luukkonen@lacdr.leidenuniv.nl>
2023-01-16 14:28:11 +0100

    remove obsolte models folder

--- drugex/training/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from ..generators.base import *
-from ..generators.sequence_rnn import *
-from ..generators.sequence_transformer import SequenceTransformer
-from ..generators.graph_transformer import GraphTransformer
