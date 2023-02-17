7adab05f1d1f4444234bdedbc6fdcb3813b0dbdf
martin-sicho <sicho.martin@gmail.com>
2023-02-17 21:27:51 +0100

    temporary fix to handle bad molecules

--- drugex/training/scorers/qsprpred.py
+++ drugex/training/scorers/qsprpred.py
@@ -4,6 +4,7 @@ qsprpred
 Created by: Martin Sicho
 On: 17.02.23, 13:44
 """
+import numpy as np
 from rdkit import Chem
 
 from drugex.training.scorers.interfaces import Scorer
@@ -16,13 +17,13 @@ class QSPRPredScorer(Scorer):
 
     def getScores(self, mols, frags=None):
         if type(mols[0]) != str:
-            mols = [Chem.MolToSmiles(mol) for mol in  mols]
+            mols = [Chem.MolToSmiles(mol) if mol else "INVALID" for mol in mols]
 
         if self.model.task == ModelTasks.REGRESSION:
             return self.model.predictMols(mols)
         else:
             # FIXME: currently we only assume that the model is a binary classifier with the positive class being the last one in the list of probabilities
-            return self.model.predictMols(mols, use_probas=True)[:,-1]
+            return np.array([i[-1] if i is not None else 0 for i in  self.model.predictMols(mols, use_probas=True)])
 
     def getKey(self):
         return f"QSPRpred_{self.model.name}"
