69c1fc5eacfe5c2f5fff693ae383baa0fb3ddad7
martin-sicho <sicho.martin@gmail.com>
2023-03-01 16:07:36 +0100

    move the generate method to the base class

--- drugex/training/generators/sequence_transformer.py
+++ drugex/training/generators/sequence_transformer.py
@@ -1,7 +1,6 @@
 import tempfile
 import torch
 
-import pandas as pd
 import torch.nn as nn
 
 from torch import optim
@@ -290,67 +289,12 @@ class SequenceTransformer(FragGenerator):
         
         return loader
 
-    def generate(self, input_frags = None, input_loader = None, num_samples=100, batch_size=32, n_proc=1,
-                keep_frags=True, drop_duplicates=True, drop_invalid=True, 
-                evaluator=None, no_multifrag_smiles=True, drop_undesired=True, raw_scores=True, compute_desirability=True,
-                progress=True, tqdm_kwargs={}):
+    def decodeLoaders(self, src, trg):
+        new_smiles = [self.voc_trg.decode(s, is_tk=False) for s in trg]
+        new_frags = [self.voc_trg.decode(s, is_tk=False) for s in src]
+        return new_frags, new_smiles
 
-        if input_loader and input_frags:
-            raise ValueError('Only one of input_loader and input_frags can be provided')
-        elif not input_loader and not input_frags:
-            raise ValueError('Either input_loader or input_frags must be provided')
-        elif input_frags:
-            # Create a dataloader object from the input fragments
-            loader = self.loaderFromFrags(input_frags, batch_size=batch_size, n_proc=n_proc)
-        else:
-            loader = input_loader
-
-        # Duplicate of self.sample to allow dropping molecules and progress bar on the fly
-        # without additional overhead caused by calling nn.DataParallel a few times
-        net = nn.DataParallel(self, device_ids=self.gpus)
-        
-        if progress:
-            tqdm_kwargs.update({'total': num_samples, 'desc': 'Generating molecules'})
-            pbar = tqdm(**tqdm_kwargs)
-
-        smiles, frags = [], []
-        while not len(smiles) >= num_samples:
-            with torch.no_grad():
-                src, _ = next(iter(loader))
-                trg = net(src.to(self.device))
-                new_smiles = [self.voc_trg.decode(s, is_tk=False) for s in trg]
-                new_frags = [self.voc_trg.decode(s, is_tk=False) for s in src]
-
-                # If drop_invalid is True, invalid (and inaccurate) SMILES are dropped
-                # valid molecules are canonicalized and optionally extra filtering is applied
-                # else invalid molecules are kept and no filtering is applied
-                if drop_invalid:
-                    new_smiles, new_frags = self.filterNewMolecules(smiles, new_smiles, new_frags, drop_duplicates=drop_duplicates, 
-                                                              drop_undesired=drop_undesired, evaluator=evaluator, no_multifrag_smiles=no_multifrag_smiles)
-
-                # Update list of smiles and frags
-                smiles += new_smiles
-                frags += new_frags
-                
-                # Update progress bar
-                if progress:
-                    pbar.update(len(new_smiles) if pbar.n + len(new_smiles) <= num_samples else num_samples - pbar.n)
-                
-        if progress:
-            pbar.close()
-        
-        smiles = smiles[:num_samples]
-        frags = frags[:num_samples]
-
-        # Post-processing
-        df_smiles = pd.DataFrame({'SMILES': smiles, 'Frags': frags})
-
-        if compute_desirability:
-            df_smiles['Desired'] = self.evaluate(smiles, frags, evaluator=evaluator, no_multifrag_smiles=no_multifrag_smiles).Desired
-        if raw_scores:
-            df_smiles = pd.concat([df_smiles, self.evaluate(smiles, frags, evaluator=evaluator, no_multifrag_smiles=no_multifrag_smiles, unmodified_scores=True)], axis=1)
-        if not keep_frags:
-            df_smiles = df_smiles.drop('Frags', axis=1)
-
-        return df_smiles
+    def iterLoader(self, loader):
+        for _, src in loader:
+            yield src
 
