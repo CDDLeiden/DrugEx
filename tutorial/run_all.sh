b46c8053225d19c11074ffb9d8e1683041a481e9
sohvi <s.luukkonen@lacdr.leidenuniv.nl>
2023-08-09 11:28:03 +0200

    add run_all.sh to tutorials

---
+++ tutorial/run_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Run all jupyter notebooks in the tutorial directory
+set -e
+
+# Install requirements
+pip install mols2grid
+pip install git+https://github.com/martin-sicho/papyrus-scaffold-visualizer.git@dev#egg=papyrus-scaffold-visualizer
+
+# Download data/models
+python -m drugex.download
+
+# Run each notebook (order matters so let's do it manually)
+jupyter nbconvert --to notebook --execute qsar.ipynb
+jupyter nbconvert --to notebook --execute Sequence-RNN.ipynb
+jupyter nbconvert --to notebook --execute Graph-Transformer.ipynb
+jupyter nbconvert --to notebook --execute advanced/extending_api.ipynb
+jupyter nbconvert --to notebook --execute advanced/scaffold_based.ipynb
