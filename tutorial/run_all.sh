8f7391ee4ce0b309e503403c15b52283ffeea390
martin-sicho <sicho.martin@gmail.com>
2023-08-14 15:34:08 +0200

    move  qsar at the end

--- tutorial/run_all.sh
+++ tutorial/run_all.sh
@@ -9,11 +9,11 @@ set -e
 # pip install git+https://github.com/martin-sicho/papyrus-scaffold-visualizer.git@dev#egg=papyrus-scaffold-visualizer
 
 # Download data/models
-# python -m drugex.download
+# python -m drugex.download (run this at least once before running the notebooks)
 
 # Run each notebook (order matters so let's do it manually)
-jupyter nbconvert --to notebook --execute qsar.ipynb
 jupyter nbconvert --to notebook --execute Sequence-RNN.ipynb
 jupyter nbconvert --to notebook --execute Graph-Transformer.ipynb
 jupyter nbconvert --to notebook --execute advanced/extending_api.ipynb
 jupyter nbconvert --to notebook --execute advanced/scaffold_based.ipynb
+jupyter nbconvert --to notebook --execute qsar.ipynb # run this last (to test the downloaded models)
