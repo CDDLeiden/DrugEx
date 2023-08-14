d36b2c2944fa2fc46609ee166c52fae225f6331b
martin-sicho <sicho.martin@gmail.com>
2023-08-14 17:39:30 +0200

    dynamic versioning

--- drugex/about.py
+++ drugex/about.py
@@ -4,5 +4,10 @@ about
 Created by: Martin Sicho
 On: 24.06.22, 10:36
 """
+import os
 
-VERSION = "3.4.4"
+VERSION = "3.4.5"
+
+if os.path.exists(os.path.join(os.path.dirname(__file__), '_version.py')):
+    from ._version import version
+    VERSION = version
