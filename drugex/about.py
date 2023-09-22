10631d3727136727501a5331f37f11379d51dea3
Martin Sicho <sicho.martin@gmail.com>
2023-09-22 13:57:12 +0200

    bump up version for next release

--- drugex/about.py
+++ drugex/about.py
@@ -6,7 +6,7 @@ On: 24.06.22, 10:36
 """
 import os
 
-VERSION = "3.4.5"
+VERSION = "3.4.6"
 
 if os.path.exists(os.path.join(os.path.dirname(__file__), '_version.py')):
     from ._version import version
