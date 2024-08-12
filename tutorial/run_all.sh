ced6179ef8bc3f3463f9600137bb50a7c4f56af5
Helle van den Maagdenberg <h.w.van.den.maagdenberg@lacdr.leidenuniv.nl>
2024-08-12 11:36:26 +0200

    uncomment extending_api

--- tutorial/run_all.sh
+++ tutorial/run_all.sh
@@ -11,7 +11,7 @@ drugex download
 # Run each notebook (order matters so let's do it manually)
 jupyter nbconvert --to notebook --execute Sequence-RNN.ipynb
 jupyter nbconvert --to notebook --execute Graph-Transformer.ipynb
-# jupyter nbconvert --to notebook --execute advanced/extending_api.ipynb
+jupyter nbconvert --to notebook --execute advanced/extending_api.ipynb
 jupyter nbconvert --to notebook --execute advanced/scaffold_based.ipynb
 jupyter nbconvert --to notebook --execute advanced/multitask_scorers.ipynb
 jupyter nbconvert --to notebook --execute qsar.ipynb # run this last (to test the downloaded models)
