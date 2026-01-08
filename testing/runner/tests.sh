122054b710726c3b20f50657b1d6f8c1280b741f
martin-sicho <sicho.martin@gmail.com>
2026-01-08 14:21:23 +0100

    fix isssue with anaconda license agreement

--- testing/runner/tests.sh
+++ testing/runner/tests.sh
@@ -9,6 +9,12 @@ RUN_CMD="${ACTIVATE_CMD} && conda activate ${ENV_NAME}"
 WD=`pwd`
 
 # setting up environments
+echo "Setting up channels..."
+conda config --remove-key channels
+conda config --add channels conda-forge
+conda config --show channels
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 echo "Creating environment: ${ENV_NAME}"
 bash -c "${ACTIVATE_CMD} && conda create -n ${ENV_NAME} python=${PYTHON_VERSION}"
 
